219db49801f4ecd11fddf4bc32f260fade8ece02
d4mi <d4mi@d4mi.pl>
2014-12-24 23:25:19 +0100

    Added initial version of notes editor

--- SecureNote/SecureNote/ViewModel/UserRegistrationViewModel.cs
+++ SecureNote/SecureNote/ViewModel/UserRegistrationViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SecureNote.ViewModel
 {
@@ -35,6 +37,24 @@ namespace SecureNote.ViewModel
             set
             {
                 base.Set(() => Password, ref _password, value);
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand _logInCommand;
+        public ICommand LogInCommand
+        {
+            get
+            {
+                return _logInCommand
+                    ?? (_logInCommand = new RelayCommand(
+                        () =>
+                        {
+
+                        }));
             }
         }
 
