13230589f45684e12cadf2aa2d3ef91cf99c8c00
d4mi <d4mi@d4mi.pl>
2014-12-25 18:14:01 +0100

    GUI updated, added ViewModel for note editor

---
+++ SecureNote/SecureNote/ViewModel/TextNoteEditorViewModel.cs
@@ -0,0 +1,53 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureNote.ViewModel
+{
+    public class TextNoteEditorViewModel : ViewModelBase
+    {
+        #region Properties
+
+        private string _titile = String.Empty;
+        public string Titile
+        {
+            get
+            {
+                return _titile;
+            }
+            set
+            {
+                base.Set(() => Titile, ref _titile, value);
+            }
+        }
+
+
+        private string _text = String.Empty;
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                base.Set(() => Text, ref _text, value);
+            }
+        }
+
+
+        #endregion
+
+        #region Ctor
+
+        public TextNoteEditorViewModel()
+        {
+
+        }
+
+        #endregion
+    }
+}
